5c5e7bb1f9b12877c213b466f97f80dc337a696b
Vivian Coday <vivian@spanishowl.com>
2019-11-24 17:37:41 -0800

    Adds code for Hexagon class.

--- Hexagon.cs
+++ Hexagon.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace FuturePunk
 {
+    class Point
+    {
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public readonly double x;
+        public readonly double y;
+    }
+    
     class Hexagon
     {
+        public Hexagon(int q, int r, int s)
+        {
+            this.q = q;
+            this.r = r;
+            this.s = s;
+
+            if (q + r + s != 0) throw new ArgumentException("q + r + s must be 0");
+        }
+        public readonly int q;
+        public readonly int r;
+        public readonly int s;
+
     }
 }
+
